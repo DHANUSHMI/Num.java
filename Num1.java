bc1c77f93839240e1af4bc9538c4087aee3bb0a9
GOWRITHIRISHA <gowridevi1997@gmail.com>
2017-06-07 17:50:14 +0530

    Update and rename README.md to Num1.java

---
+++ Num1.java
@@ -0,0 +1,22 @@
+# Num.java
+import java.io.*;
+import java.util.Scanner;
+class Num
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int s=sc.nextInt();
+{
+if(n>0)
+{
+System.out.println("Positive");
+}
+else
+{
+System.out.println("Negative");
+}
+}
+}
+}
+
